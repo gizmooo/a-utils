bc7d2fcd02ad21ef1a7e1d59bcc65e0f3a91b727
Anton Ustinov <ustin_moto@mail.ru>
2021-06-15 15:48:31 +0300

    update typescript version

--- src/easings/easings.d.ts
+++ src/easings/easings.d.ts
@@ -1,15 +1,13 @@
-export declare const Ease: {
-    linear: (t: number) => number;
-    easeInQuad: (t: number) => number;
-    easeOutQuad: (t: number) => number;
-    easeInOutQuad: (t: number) => number;
-    easeInCubic: (t: number) => number;
-    easeOutCubic: (t: number) => number;
-    easeInOutCubic: (t: number) => number;
-    easeInQuart: (t: number) => number;
-    easeOutQuart: (t: number) => number;
-    easeInOutQuart: (t: number) => number;
-    easeInQuint: (t: number) => number;
-    easeOutQuint: (t: number) => number;
-    easeInOutQuint: (t: number) => number;
-};
+export declare const linear: (t: number) => number;
+export declare const easeInQuad: (t: number) => number;
+export declare const easeOutQuad: (t: number) => number;
+export declare const easeInOutQuad: (t: number) => number;
+export declare const easeInCubic: (t: number) => number;
+export declare const easeOutCubic: (t: number) => number;
+export declare const easeInOutCubic: (t: number) => number;
+export declare const easeInQuart: (t: number) => number;
+export declare const easeOutQuart: (t: number) => number;
+export declare const easeInOutQuart: (t: number) => number;
+export declare const easeInQuint: (t: number) => number;
+export declare const easeOutQuint: (t: number) => number;
+export declare const easeInOutQuint: (t: number) => number;
--- src/easings/easings.ts
+++ src/easings/easings.ts
@@ -1,28 +1,26 @@
-export const Ease = {
-  // no easing, no acceleration
-  linear: (t: number): number => t,
-  // accelerating from zero velocity
-  easeInQuad: (t: number): number => t*t,
-  // decelerating to zero velocity
-  easeOutQuad: (t: number): number => t*(2-t),
-  // acceleration until halfway, then deceleration
-  easeInOutQuad: (t: number): number => t<.5 ? 2*t*t : -1+(4-2*t)*t,
-  // accelerating from zero velocity
-  easeInCubic: (t: number): number => t*t*t,
-  // decelerating to zero velocity
-  easeOutCubic: (t: number): number => (--t)*t*t+1,
-  // acceleration until halfway, then deceleration
-  easeInOutCubic: (t: number): number => t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1,
-  // accelerating from zero velocity
-  easeInQuart: (t: number): number => t*t*t*t,
-  // decelerating to zero velocity
-  easeOutQuart: (t: number): number => 1-(--t)*t*t*t,
-  // acceleration until halfway, then deceleration
-  easeInOutQuart: (t: number): number => t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t,
-  // accelerating from zero velocity
-  easeInQuint: (t: number): number => t*t*t*t*t,
-  // decelerating to zero velocity
-  easeOutQuint: (t: number): number => 1+(--t)*t*t*t*t,
-  // acceleration until halfway, then deceleration
-  easeInOutQuint: (t: number): number => t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t
-}
+// no easing, no acceleration
+export const linear = (t: number): number => t;
+// accelerating from zero velocity
+export const easeInQuad = (t: number): number => t*t;
+// decelerating to zero velocity
+export const easeOutQuad = (t: number): number => t*(2-t);
+// acceleration until halfway, then deceleration
+export const easeInOutQuad = (t: number): number => t<.5 ? 2*t*t : -1+(4-2*t)*t;
+// accelerating from zero velocity
+export const easeInCubic = (t: number): number => t*t*t;
+// decelerating to zero velocity
+export const easeOutCubic = (t: number): number => (--t)*t*t+1;
+// acceleration until halfway, then deceleration
+export const easeInOutCubic = (t: number): number => t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1;
+// accelerating from zero velocity
+export const easeInQuart = (t: number): number => t*t*t*t;
+// decelerating to zero velocity
+export const easeOutQuart = (t: number): number => 1-(--t)*t*t*t;
+// acceleration until halfway, then deceleration
+export const easeInOutQuart = (t: number): number => t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t;
+// accelerating from zero velocity
+export const easeInQuint = (t: number): number => t*t*t*t*t;
+// decelerating to zero velocity
+export const easeOutQuint = (t: number): number => 1+(--t)*t*t*t*t;
+// acceleration until halfway, then deceleration
+export const easeInOutQuint = (t: number): number => t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t;
