c3d6e13a0a24643c93679b760e6d59e8c9698f79
Anton Ustinov <ustin_moto@mail.ru>
2021-08-17 20:33:59 +0300

    fix mapObjectToParamsString

--- src/http/mapObjectToParamsString.js
+++ src/http/mapObjectToParamsString.js
@@ -1,9 +1,9 @@
 export const mapObjectToParamsString = (params) => {
-    if (!params)
+    if (!params || !Object.entries(params).length)
         return '';
     let paramsString = '?';
     for (let key in params) {
-        if (params.hasOwnProperty(key) && typeof params[key] !== 'undefined')
+        if (params.hasOwnProperty(key) && typeof params[key] !== 'undefined' && params[key] !== null)
             paramsString += key + '=' + params[key].toString() + '&';
     }
     return paramsString.substring(0, paramsString.length - 1);
--- src/http/mapObjectToParamsString.ts
+++ src/http/mapObjectToParamsString.ts
@@ -1,10 +1,13 @@
-export const mapObjectToParamsString = <T extends { [key: string]: any } & (keyof T extends string ? {} : 'T must only have string keys')>(params?: T): string => {
-  if (!params) return '';
+export const mapObjectToParamsString = <T extends { [key: string]: any } &
+  (keyof T extends string ? {} : 'T must only have string keys')>(params?: T): string => {
+
+  if (!params || !Object.entries(params).length) return '';
 
   let paramsString = '?';
 
   for (let key in params) {
-    if (params.hasOwnProperty(key) && typeof params[key] !== 'undefined') paramsString += key + '=' + params[key].toString() + '&';
+    if (params.hasOwnProperty(key) && typeof params[key] !== 'undefined' && params[key] !== null)
+      paramsString += key + '=' + params[key].toString() + '&';
   }
 
   return paramsString.substring(0, paramsString.length - 1);
